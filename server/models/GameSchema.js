9a9b81f1a8d771870265e544c5682a73f4c1eba0
Hertafeld <hertafeld@gmail.com>
2020-07-24 01:01:19 -0700

    Updated package with react build

--- server/models/GameSchema.js
+++
@@ -1,9 +0,0 @@
-const  mongoose  = require("mongoose");
-const  Schema  =  mongoose.Schema;
-const  gameSchema  =  new Schema({
-    id:  String,
-    subgames: [{history: [String], weight: Number, angle: Number, winner: String}]
-});
-let  Game  =  mongoose.model("Game", gameSchema);
-
-module.exports  =  Game;
